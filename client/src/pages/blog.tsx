f32370ce0062da8571654417462581abd8559886
Tirasp0l <sl0271705@gmail.com>
2026-02-26 21:34:59 +0200

    Update blog.tsx

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -35,15 +35,15 @@ Before filtering, you need to know what you are dealing with.
 
   - **Protocol Hierarchy (What type of traffic do you have?):**
   
-    \`tshark -r fisier.pcap -z io,phs\`
+      \`tshark -r fisier.pcap -z io,phs\`
 
   - **Listing IP Addresses (Endpoints):**
   
-    \`tshark -r fisier.pcap -z endpoints,ip\`
+      \`tshark -r fisier.pcap -z endpoints,ip\`
 
   - **Displaying the first 10 packets (Summary):**
   
-    \`tshark -r fisier.pcap -c 10\`
+      \`tshark -r fisier.pcap -c 10\`
 
 ### 2. Data Extraction (Field Filters)
 
@@ -51,15 +51,15 @@ The most useful feature for reporting or scripting. We use \`-T\` fields to choo
 
   - **Extract Source, Destination and Port IP:**
   
-    \`tshark -r fisier.pcap -T fields -e ip.src -e ip.dst -e tcp.dstport -E header=y\`
+      \`tshark -r fisier.pcap -T fields -e ip.src -e ip.dst -e tcp.dstport -E header=y\`
 
   - **Extract Visited URLs (HTTP):**
   
-    \`tshark -r fisier.pcap -Y http.request -T fields -e http.host -e http.request.uri\`
+      \`tshark -r fisier.pcap -Y http.request -T fields -e http.host -e http.request.uri\`
 
   - **Extract DNS queries:**
   
-    \`tshark -r fisier.pcap -T fields -e dns.qry.name | sort | uniq -c\`
+      \`tshark -r fisier.pcap -T fields -e dns.qry.name | sort | uniq -c\`
 
 `
   },
