b4fa2a7dd9b2933a6fcbec461bdf169483adbf15
Tirasp0l <sl0271705@gmail.com>
2026-04-26 17:21:40 +0300

    Update chimie.tsx

--- client/src/pages/chimie.tsx
+++ client/src/pages/chimie.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 const Chimie = () => {
   return (
     <div>
-      <h1 class="text-center">Săpunurile: Chimie Organică</h1>
+      <br/><br/>
+    
+      <h1 class="text-center text-[72px]">Săpunurile: Chimie Organică</h1>
 
-      <iframe class="content-center" width="1280" height="720" 
-        src="https://www.youtube.com/embed/UV1OchR0Qdg?si=u7PaRn_2rPxJs1RW" 
-        title="YouTube video player" frameborder="0" allow="accelerometer; 
-        autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
-        web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-      </iframe>
-      
+      <br/><br/>
+
+      <div class="content-center">
+        <iframe width="1280" height="720" 
+          src="https://www.youtube.com/embed/UV1OchR0Qdg?si=u7PaRn_2rPxJs1RW" 
+          title="YouTube video player" frameborder="0" allow="accelerometer; 
+          autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
+          web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
+      </div>
       
     </div>
   );
