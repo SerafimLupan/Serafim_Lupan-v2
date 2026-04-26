b3d5c07a2ab73c55fc780a8b0f091d30400053c4
Tirasp0l <sl0271705@gmail.com>
2026-04-26 16:59:39 +0300

    Add Chimie component for chemistry resources page

---
+++ client/src/pages/chimie.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chimie = () => {
+  return (
+    <div>
+      <h1>Pagina de Chimie</h1>
+      <p>Aici vei adăuga resursele pentru chimie.</p>
+    </div>
+  );
+};
+
+export default Chimie;
