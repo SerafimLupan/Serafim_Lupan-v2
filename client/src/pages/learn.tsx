e9e4309e3da98b6bc8b06bce585189424b4cb863
Tirasp0l <sl0271705@gmail.com>
2026-05-11 15:56:22 +0300

    Update learn.tsx

--- client/src/pages/learn.tsx
+++ client/src/pages/learn.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 
-import { MatrixBackground } from "@/components/MatrixBackground";
-import { Card } from "@/components/Card";
-import { motion } from "framer-motion";
-import { useState } from "react";
-import { Copy, Check } from "lucide-react";
-import { useToast } from "@/hooks/use-toast";
-
-
 const learn = () => {
   return (
     <h1 className="text-center text-[40px] md:text-[72px] font-bold my-10 text-green-500">
         90 days of cybersecurity
-      </h1
+      </h1>
   );  
 };
 
