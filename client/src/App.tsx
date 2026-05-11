fe038145eb1c9181bfbd0ea241ed173a8baf7f70
Tirasp0l <sl0271705@gmail.com>
2026-05-11 14:22:53 +0300

    Add route for 90learnOfCS page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,15 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Home from "@/pages/Home";
 import Blog from "@/pages/blog"; 
+import chimie from "@/pages/chimie";
+import 90learnOfCS from "@/pages/90learn";
+
 import NotFound from "@/pages/not-found";
-import chimie from "@/pages/chimie"; 
+ 
+
 
 function Router() {
   return (
@@ -14,6 +19,7 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/blog" component={Blog} /> 
       <Route path="/chimie" component={chimie} />
+      <Route path="/90learnOfCS" component={90learnOfCS} />
       <Route component={NotFound} />
     </Switch>
   );
