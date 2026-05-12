0bd091e3b336c0bea159b279e40272c30a94809a
Tirasp0l <sl0271705@gmail.com>
2026-05-12 11:51:01 +0300

    Update blog.tsx

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -56,6 +56,17 @@ interface BlogPost {
 
 const BLOG_POSTS: BlogPost[] = [
   {
+    id: "11",
+    title: "My progress in CYBERSECURITY",
+    date: "2025-05-12 12:30 PM",
+    summary: "What is going on with my CYBERSECURITY learning journey? I participated at Cyber ​​Security Olympiad",
+    content: `
+On **April 24**, **2026**, I participated in the **Cyber ​​Security Olympiad**, achieving a score of **42.7117 out of 100**. To qualify for the national stage, I had to have a **minimum of 60 out of 100**.
+
+The results that will show my place in the 10th grade rankings for Suceava County, Romania
+      `
+  },
+  {
     id: "10",
     title: "My progress in CYBERSECURITY",
     date: "2026-03-09 12:30 PM",
