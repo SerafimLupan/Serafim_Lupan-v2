fe6277206d6cc26a3a92b28184c7e5e86e6d5cba
Tirasp0l <sl0271705@gmail.com>
2026-08-05 11:43:16 +0000

    Modify REWERS Barbershop project details
    Updated project details for REWERS Barbershop, including a new URL and a more descriptive note on its status.

--- client/src/data.ts
+++ client/src/data.ts
@@ -124,14 +124,12 @@ P"Ybmmd"   \`Mbmmd' .JMML.   \`Moo9^Yo..JMML.  .JMML..JMML  JMML  JMML.    .JMMm
   },
 
   projects: [
-   /* 
    {
       name: "REWERS Barbershop",
-      url: "https://www.serafimlupan.site/projects/Rewers%20BARBERSHOP%20website/index.html",  // Invalid address
+      url: "https://serafimlupan.github.io/Serafim_Lupan-v1/projects/Rewers%20BARBERSHOP%20website/index.html",
       date: "12.2023 - 01.2024",
-      description: "Modern layout and booking section."
+      description: "Modern layout and booking section. Not finished because of the barbershop owner."
     }, 
-    */
     {
       name: "TIFS Project",
       url: "https://serafimlupan.github.io/DezvoltareWeb/",
