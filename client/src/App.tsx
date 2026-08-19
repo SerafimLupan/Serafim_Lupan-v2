5ad2768cad191cd8e8fb6ae84968e02f77e04410
Tirasp0l <tirasp0l@kali.serafimlupan.com>
2026-08-19 19:36:38 +0100

    adding page bpriucs.tsx to redirect a link from linkedin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 import Home from "@/pages/Home";
-import Blog from "@/pages/blog"; 
+import Blog from "@/pages/blog";
 import Learn from "@/pages/learn";
 import NotFound from "@/pages/not-found";
-
-
+import BpriucsRedirect from "@/pages/bpriucs";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/blog" component={Blog} /> 
+      <Route path="/blog" component={Blog} />
       <Route path="/learn" component={Learn} />
+      <Route path="/bpriucs" component={BpriucsRedirect} />
       <Route component={NotFound} />
     </Switch>
   );
---
+++ client/src/pages/bpriucs.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+export default function BpriucsRedirect() {
+        useEffect(() => {
+        window.location.replace(
+            'https://bprcauseni.wordpress.com/2021/03/18/youthub/#:~:text=Serafim%20Lupan'
+        );
+    }, []);
+
+    return null;
+}
